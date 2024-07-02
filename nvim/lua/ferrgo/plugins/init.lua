88f3aecfd591992e50ded0dd0ff8ad61659356a3
Hugo Gouveia <hgchrome@gmail.com>
2024-07-02 02:39:05 -0300

    fix: add copilot and bring back tabs? wat?

--- nvim/lua/ferrgo/plugins/init.lua
+++ nvim/lua/ferrgo/plugins/init.lua
@@ -1,30 +1,30 @@
 return {
-	{ import = "ferrgo.plugins.lsp" },
---	"github/copilot.vim",
-	{
-		"prichrd/netrw.nvim",
-		config = function()
-			require("netrw").setup({
-				use_devicons = true,
-				icons = {
-					symlink = "", -- Symlink icon (directory and file)
-					directory = "", -- Directory icon
-					file = "", -- File icon
-				},
-			})
-		end,
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
+    { import = "ferrgo.plugins.lsp" },
+    "github/copilot.vim",
+    {
+        "prichrd/netrw.nvim",
+        config = function()
+            require("netrw").setup({
+                use_devicons = true,
+                icons = {
+                    symlink = "", -- Symlink icon (directory and file)
+                    directory = "", -- Directory icon
+                    file = "", -- File icon
+                },
+            })
+        end,
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
 }

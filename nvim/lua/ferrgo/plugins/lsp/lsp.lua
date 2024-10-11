a40735949edd95135d9aee8c6048727929ed5e1a
Hugo Gouveia <hgchrome@gmail.com>
2024-10-11 15:30:04 -0300

    fix(nvim): lsp tsserver rename
    See [PR](https://github.com/neovim/nvim-lspconfig/pull/3232)

--- nvim/lua/ferrgo/plugins/lsp/lsp.lua
+++ nvim/lua/ferrgo/plugins/lsp/lsp.lua
@@ -39,7 +39,7 @@ return {
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
-                    "tsserver",
+                    "ts_ls",
                     "eslint",
                     "bashls",
                     "yamlls",

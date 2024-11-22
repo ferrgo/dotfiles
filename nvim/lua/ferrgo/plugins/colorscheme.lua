b80179c629daeb4c4574b719b3a7b6ddcd623015
Hugo Gouveia <hgchrome@gmail.com>
2024-11-22 19:48:03 -0300

    feat(nvim): add sonarlint

--- nvim/lua/ferrgo/plugins/colorscheme.lua
+++ nvim/lua/ferrgo/plugins/colorscheme.lua
@@ -13,8 +13,9 @@ return {
                 -- your configuration comes here
                 -- or leave it empty to use the default settings
                 style = "night",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true,     -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                transparent = false,     -- Enable this to disable setting the background color
+--                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                dim_inactive = false,
                 styles = {
                     -- Style to be applied to different syntax groups
                     -- Value is any valid attr-list value for `:help nvim_set_hl`

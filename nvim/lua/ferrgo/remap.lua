a9c03803bf9810f3477982556affeb85c5e41a70
Hugo Gouveia <hgchrome@gmail.com>
2025-07-11 00:26:17 -0300

    feat(nvim): remaps for term and quickfix

--- nvim/lua/ferrgo/remap.lua
+++ nvim/lua/ferrgo/remap.lua
@@ -1,4 +1,4 @@
-vim.g.mapleader=" "
+vim.g.mapleader = " "
 
 -- Disabled to use Oil
 -- vim.keymap.set("n","<leader>ls", vim.cmd.Ex, {desc="Open current folder in explorer"})
@@ -13,6 +13,9 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- Terminal
+vim.keymap.set("t", "<ESC><ESC>", "<C-\\><C-n>")
+
 -- Thanks [ThePrimeagen](https://github.com/ThePrimeagen/init.lua/blob/249f3b14cc517202c80c6babd0f9ec548351ec71/lua/theprimeagen/remap.lua#L21-L22)
 -- "_d <- delete to blackhole register
 -- P <- paste before cursor
@@ -20,13 +23,13 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- Thanks asbjornHaland and [ThePrimeagen](https://github.com/ThePrimeagen/init.lua/blob/249f3b14cc517202c80c6babd0f9ec548351ec71/lua/theprimeagen/remap.lua#L24-L26)
--- "+y <- yank selection into "+" register (System clipboard) 
+-- "+y <- yank selection into "+" register (System clipboard)
 -- "+Y <- yank entire line into "+" register (System clipboard)
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- Quickfix navigation
-vim.keymap.set("n", "[q","<Cmd>cp<CR>",{desc="[Quickfix navigation] Next"})
-vim.keymap.set("n", "]q","<Cmd>cn<CR>",{desc="[Quickfix navigation] Previous"})
-vim.keymap.set("n", "[Q","<Cmd>cr<CR>",{desc="[Quickfix navigation] First"})
-vim.keymap.set("n", "]Q","<Cmd>cla<CR>",{desc="[Quickfix navigation] Last"})
+vim.keymap.set("n", "[q", "<Cmd>cp<CR>", { desc = "[Quickfix navigation] Next" })
+vim.keymap.set("n", "]q", "<Cmd>cn<CR>", { desc = "[Quickfix navigation] Previous" })
+vim.keymap.set("n", "[Q", "<Cmd>cr<CR>", { desc = "[Quickfix navigation] First" })
+vim.keymap.set("n", "]Q", "<Cmd>cla<CR>", { desc = "[Quickfix navigation] Last" })
